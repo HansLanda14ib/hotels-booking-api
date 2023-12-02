6fc9829e47f0e7de04953be37b34d863feb61da1
Hansa Landa <bibzazne@gmail.com>
2023-12-02 23:06:28 +0100

    testing another api gateway approach

--- api-gateway/index.js
+++ api-gateway/index.js
@@ -1,17 +1,16 @@
 const express = require("express");
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const {createProxyMiddleware} = require('http-proxy-middleware');
 require('dotenv').config();
 const app = express();
 const service_hotel_port = process.env.SERVICE_HOTEL_PORT || 5001;
-const service_booking_port = process.env.SERVICE_BOOKING_PORT  || 5002;
-const api_gateway_port =process.env.API_GATEWAY_PORT || 5555;
+const service_booking_port = process.env.SERVICE_BOOKING_PORT || 5002;
+const api_gateway_port = process.env.API_GATEWAY_PORT || 5555;
 // Route requests to the front-end service
-let hotelApi,BookingsApi;
+let hotelApi, BookingsApi;
 if (process.env.NODE_ENV === 'production') {
     hotelApi = process.env.HOTEL_PROD_URL;
     BookingsApi = process.env.BOOKING_PROD_URL;
-}
-else {
+} else {
     hotelApi = process.env.HOTEL_DEV_URL;
     BookingsApi = process.env.BOOKING_DEV_URL;
 
@@ -19,12 +18,18 @@ else {
 // Route requests to the product service
 app.use("/api/v1/hotels", createProxyMiddleware({
     target: hotelApi,
-    changeOrigin: true
+    changeOrigin: true,
+    pathRewrite: {
+        '^/api/v1/hotels': '', // Remove the /api/v1/hotels prefix from the URL
+    },
 }));
 // Route requests to the booking service
 app.use("/api/v1/bookings", createProxyMiddleware({
     target: BookingsApi,
-    changeOrigin: true
+    changeOrigin: true,
+    pathRewrite: {
+        '^/api/v1/bookings': '', // Remove the /api/v1/bookings prefix from the URL
+    },
 }));
 
 
